a6c31e4a18fef593d92257349767c6e10e9258ce
FacuEt <facurawson@gmail.com>
2016-09-08 12:42:01 -0300

    Adapto db para que se pueda utilizar desde heroku y docker sin tener que hacer cambios manuales

---
+++ src/config/configdb.js
@@ -0,0 +1,29 @@
+exports.jsonConfigDB = function(){
+	var json_db = {};
+
+	//si está definido es docker
+	if (typeof process.env['PGDATABASE'] !== 'undefined'){
+		json_db = {
+	    	"name"		  : "DOCKER",
+	        "db"          : process.env['PGDATABASE'],
+	        "user"        : process.env['PGUSER_USER'],
+	        "password"    : process.env['PGUSER_PASSWORD'],
+	        "host"        : process.env['DATABASE_URL'],
+	        "timeToWaitDB": 10000,
+	        "addons"      : ""
+	    };
+
+	} else { // es heroku
+		json_db = {
+	    	"name"		  : "HEROKU",
+	        "db"          : "d5uretjrtlehmp",
+	        "user"        : "lthaghpcskkrpu",
+	        "password"    : "4PKMzgLy-_Dvlx-FZktuNQivSN",
+	        "host"        : "ec2-54-221-245-174.compute-1.amazonaws.com:5432",
+	        "timeToWaitDB": 0,
+	        "addons"      : "?ssl=true"
+	    };
+	}
+
+	return json_db;
+};
