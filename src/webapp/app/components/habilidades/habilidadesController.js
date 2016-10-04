aa2b2f559973a756b7cc6bb5a9977380ca588422
juanmamanuel.bouvier <Buby24993>
2016-10-04 17:58:52 -0300

    Added confirmation dialog before deleting skill!

--- src/webapp/app/components/habilidades/habilidadesController.js
+++ src/webapp/app/components/habilidades/habilidadesController.js
@@ -1,4 +1,4 @@
-appmodule.controller('habilidadesController', function($scope, $http){
+appmodule.controller('habilidadesController', function($scope, $http, $mdDialog){
 	$scope.title="Habilidades";
 
 	$scope.skills=[];
@@ -17,17 +17,36 @@ appmodule.controller('habilidadesController', function($scope, $http){
         );
     };
 
-    $scope.deleteSkill = function(category, name) {
+    function deleteSkill(category, name) {
         $http.delete('/skills/categories/' + category + '/' + name)
-            .then(function (response) {
-                // just obtain the promise data
-                console.log(response);
-                getAllSkills();
-            }, function (err) {
-                // error handling
-                console.log(response);
-                console.log("Error on deleting skill " + name + " from category " + category);
-            }
-        );
+            .then(
+                function (response) {
+                    // just obtain the promise data
+                    console.log(response);
+                    getAllSkills();
+                }, function (err) {
+                    // error handling
+                    console.log(response);
+                    console.log("Error on deleting skill " + name + " from category " + category);
+                }
+            );
+    };
+
+    $scope.showConfirmDeleteSkill = function(ev, skill) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+              .title('¿Estás seguro de querer borrar la habilidad?')
+              .textContent('La habilidad será removida definitivamente de la base de datos.')
+              .ariaLabel('Delete skill')
+              .targetEvent(ev)
+              .ok('Borrar')
+              .cancel('Cancelar');
+
+        $mdDialog.show(confirm).then(function() {
+          console.log("The selected skill will be deleted! Skill: " + skill);
+          deleteSkill(skill.category, skill.name);
+        }, function() {
+          console.log("The skill won't be deleted.");
+        });
     };
 });
