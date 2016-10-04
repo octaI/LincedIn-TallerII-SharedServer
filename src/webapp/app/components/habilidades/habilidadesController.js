6b389114430bcb7f9280119794f130a0c5272fb7
juanmamanuel.bouvier <Buby24993>
2016-10-04 17:19:06 -0300

    Added functionality to delete skill button in Skills.

--- src/webapp/app/components/habilidades/habilidadesController.js
+++ src/webapp/app/components/habilidades/habilidadesController.js
@@ -3,7 +3,6 @@ appmodule.controller('habilidadesController', function($scope, $http){
 
 	$scope.skills=[];
 	getAllSkills();
-
 	function getAllSkills() {
     	$http.get('/skills')
         	.then(function (response) {
@@ -12,8 +11,23 @@ appmodule.controller('habilidadesController', function($scope, $http){
             	$scope.skills=response.data.skills;
         	}, function (err) {
             	// error handling
-            	console.log("Error on retrieving skills");
+                console.log(response);
+            	console.log("Error on retrieving all skills");
         	}
+        );
+    };
+
+    $scope.deleteSkill = function(category, name) {
+        $http.delete('/skills/categories/' + category + '/' + name)
+            .then(function (response) {
+                // just obtain the promise data
+                console.log(response);
+                getAllSkills();
+            }, function (err) {
+                // error handling
+                console.log(response);
+                console.log("Error on deleting skill " + name + " from category " + category);
+            }
         );
     };
 });
