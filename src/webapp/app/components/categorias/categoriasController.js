6a45976fad8569efe7361a08011531bdbbaaea3f
Octavio <octaviomdq93@gmail.com>
2016-10-06 01:44:48 -0300

    Added edit button to categories

--- src/webapp/app/components/categorias/categoriasController.js
+++ src/webapp/app/components/categorias/categoriasController.js
@@ -1,50 +1,88 @@
-appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$route){
+appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$filter){
 	$scope.title="Categorias";
-	$scope.categories = [];	
+	
 	$scope.isLoading=true;
+
+	$scope.categories = [];	
+
 	getAllCategories();
 
 	function getAllCategories(){
-		$http({
-		method: 'GET',
-		url: '/categories'
-	})
+	$http.get('/categories')
 	.then(function(response){
 		$scope.categories=response.data.categories;
 		$scope.isLoading=false;
-	},function handleError(response){
+	},function (err){
 		$scope.isLoading=false;
 		alert("An error has occured while handling the request");
-	});
+	}
+	);
 	};
 
 	
-	$scope.submitCategory = function(ev){
+	$scope.submitCategory = function(ev) {
 		$mdDialog.show({
 			controller: dialogController,
-      		template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname" md-maxlength="50" maxlength="50" placeholder="Ingrese el nombre de la categoria"> </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="addCategory(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
+      		template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname" md-maxlength="50" maxlength="50" placeholder="Ingrese el nombre de la categoria"> </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
 			targetEvent: ev,
+			clickOutsideToClose: true
 		})
-		.then(function(category){
-			var data = {
-				name : category.categoryname,
-				description: category.categorydescription
-			};
-			
-			$http.post('/categories',data)
-			.then(function(response){
-				console.log("Category submitted");
-			},function(error){
-				console.log("user canceled the input");
-			})
+		.then(function(answer) {
+			if(answer !== 'not useful'){
+				category = {'name':answer.categoryname,'description':answer.categorydescription };
+				addCategory(category);
+			}
 		},function(){
-			$scope.alert="An error occured";
+
 		});
-		
-			getAllCategories();
+	
 	};
 
-	 function deleteCategory(name,$http) {
+	$scope.showEditCategoryDialogue = function(ev, category) {
+		
+		$mdDialog.show({
+			locals:{name : category.name,description: category.description},
+			controller: dialogController,
+			template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname"  md-maxlength="50" maxlength="50" > </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" value="category.description" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
+			targetEvent:ev,
+			clickOutsideToClose:true
+		})
+		.then(function(answer){
+			if(answer !== 'not useful'){
+				editedcategory = {'name':answer.categoryname,'description':answer.categorydescription };
+				editCategory(editedcategory,category.name);
+			}
+		},function(){
+
+		});
+	}
+
+	function editCategory(category,oldcategoryname){
+		$scope.isLoading=true;
+		console.log(oldcategoryname);
+		$http.put('/categories/' + oldcategoryname,category)
+		.then(function(response){
+			getAllCategories();
+		},function(err){
+			$scope.isLoading=false;
+			alert("An error has occurred while handling the request");
+		});
+	};
+
+	function addCategory(category){
+		$scope.isLoading=true;
+		$http.post('/categories',category)
+		.then(function(response){
+			$scope.categories.push(category);
+			getAllCategories();			
+		},function(err){
+			$scope.isLoading=false;
+			alert('An error has occurred while handling the request');
+		}
+		);
+	};
+
+	function deleteCategory(name,$http) {
 	 	$scope.isLoading=true;
 	 	console.log('categories/'+name);
       	$http({
@@ -64,7 +102,6 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$rou
   };
 
 	$scope.showConfirmDeleteCategory = function(ev, category) {
-      // Appending dialog to document.body to cover sidenav in docs app
       var confirm = $mdDialog.confirm()
             .title('¿Estás seguro de querer borrar la Categoria?')
             .textContent('La categoria será removida definitivamente de la base de datos.')
@@ -80,9 +117,10 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$rou
   };
 	
 
-});
+function dialogController($scope,$mdDialog,name,description){
+		console.log(name);
+		$scope.category = {categoryname: name, categorydescription: description};
 
-function dialogController($scope,$mdDialog){
 		$scope.hide = function(){
 			$mdDialog.hide();
 		};
@@ -93,3 +131,8 @@ function dialogController($scope,$mdDialog){
 			$mdDialog.hide(answer);
 		};
 	};
+
+
+
+});
+
