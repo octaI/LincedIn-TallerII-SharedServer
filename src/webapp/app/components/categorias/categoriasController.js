cae81bf28b9572a89a5ae3d5e238eec33d486e30
Octavio <octaviomdq93@gmail.com>
2016-10-06 01:54:08 -0300

    small fix in edit

--- src/webapp/app/components/categorias/categoriasController.js
+++ src/webapp/app/components/categorias/categoriasController.js
@@ -42,7 +42,7 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$fil
 		
 		$mdDialog.show({
 			locals:{name : category.name,description: category.description},
-			controller: dialogController,
+			controller: dialogController2,
 			template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname"  md-maxlength="50" maxlength="50" > </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" value="category.description" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
 			targetEvent:ev,
 			clickOutsideToClose:true
