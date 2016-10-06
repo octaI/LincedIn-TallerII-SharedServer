4bf3e6a646d956f1cc6c4e0b17e0aa546fc3c496
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-10-05 22:59:35 -0300

    Changed color palette

--- src/webapp/app/app.theme.js
+++ src/webapp/app/app.theme.js
@@ -1,6 +1,12 @@
 appmodule.config(function($mdThemingProvider){
 	$mdThemingProvider.theme('default')
-		.primaryPalette('orange')
-		.accentPalette('green')
+		.primaryPalette('orange', {
+      		'default': '600',
+      		'hue-1': '200',
+      		'hue-2': '800'
+    	})
+		.accentPalette('green', {
+      		'default': 'A400'
+    	})
 		.warnPalette('deep-orange');
 });
