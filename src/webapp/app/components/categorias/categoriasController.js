476e836ce8963d97d8e8aa5df0683434baac2db7
Octavio <octaviomdq93@gmail.com>
2016-10-06 14:45:07 -0300

    WIP refresh when adding category

--- src/webapp/app/components/categorias/categoriasController.js
+++ src/webapp/app/components/categorias/categoriasController.js
@@ -1,11 +1,13 @@
-appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdToast){
+appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdToast,$window,$location){
 	$scope.title="Categorias";
 	
 	$scope.isLoading=true;
 
-	$scope.categories = [];	
+	$scope.categories=[];	
 
 	getAllCategories();
+
+
 
 	function getAllCategories() {
       $scope.isLoading = true;
@@ -20,35 +22,18 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdT
   };
 
 	
-	$scope.submitCategory = function(ev) {
-		$mdDialog.show({
-			controller: dialogController,
-      		template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname" md-maxlength="50" maxlength="50" placeholder="Ingrese el nombre de la categoria"> </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
-			targetEvent: ev,
-			clickOutsideToClose: true
-		})
-		.then(function(answer) {
-			if(answer !== 'not useful'){
-				category = {'name':answer.categoryname,'description':answer.categorydescription };
-				addCategory(category);
-			}
-		},function(){
 
-		});
-	
-	};
 
 	$scope.showAddCategoryDialog = function(ev) {
     $mdDialog.show({
       controller: dialogController,
-      template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname" md-maxlength="50" maxlength="50" placeholder="Ingrese el nombre de la categoria"> </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
+      template: '<md-dialog aria-label="Add skill"> <div class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.name" md-maxlength="50" maxlength="50" placeholder="Nombre de la Categoria"> </md-input-container>    <md-input-container flex> <label>Descripción</label> <textarea ng-model="category.description" columns="1" md-maxlength="150" maxlength="150" placeholder="Descripción de la categoría"></textarea> </md-input-container> </form> </div> <md-dialog-actions layout="row"> <md-button ng-click="answer(\'not useful\')" class="md-warn"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </md-dialog-actions></md-dialog>',
       targetEvent: ev,
       clickOutsideToClose: true
     })
     .then(function(answer) {
       if (answer !== 'not useful') {
-        category = {'name': answer.categoryname, 'description': answer.categorydescription};
-        console.log(category);
+        category = {'name': answer.name, 'description': answer.description};
         addCategory(category);
       }
     },function(){
@@ -60,7 +45,7 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdT
 		$mdDialog.show({
 			locals:{name : category.name,description: category.description},
 			controller: dialogController2,
-			template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname"  md-maxlength="50" maxlength="50" > </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" value="category.description" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-accent"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
+			template: '<md-dialog aria-label="Add Category"> <md-content class="md-padding"> <form name="categoryForm"> <div layout="column" layout-sm="column"> <md-input-container flex> <label>Categoria</label> <input ng-model="category.categoryname"  md-maxlength="50" maxlength="50" > </md-input-container>  <md-input-container flex> <label>Descripcion de la categoria</label> <textarea ng-model="category.categorydescription" value="category.description" columns="1" md-maxlength="150" maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')" class="md-warn"> Cancelar </md-button> <md-button ng-click="answer(category)" class="md-primary"> Guardar </md-button> </div></md-dialog>',
 			targetEvent:ev,
 			clickOutsideToClose:true
 		})
@@ -79,33 +64,32 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdT
 		console.log(oldcategoryname);
 		$http.put('/categories/' + oldcategoryname,category)
 		.then(function(response){
+			showSimpleToast("La categoria "+oldcategoryname+" ha sido editada con éxito");
 			getAllCategories();
 		},function(err){
 			console.log(err);
 			$scope.isLoading=false;
-			var toast = $mdToast.simple()
-			.textContent(err.data.error)
-			.action("OK")
-			.highlightAction(true)
-			.position("top right end");
-			$mdToast.show(toast)
-			.then(function(response){
-
-			})
+		
 		});
 	};
 
-	function addCategory(category) {
-    $scope.isLoading = true;
-    $http.post('/categories', category)
-      .then(function (response) {
-          $scope.categories.push(category);
-          getAllCategories();
-        }, function (err) {
-          $scope.isLoading = false;
-        }
-      );
-  	};
+
+
+  	function addCategory(category) {
+	    $scope.isLoading = true;
+	    $http.post('/categories', category)
+	      .then(function (response) {
+	      	 $scope.categories.push(response.data);
+	      	 getAllCategories();
+	      	 $location.path();
+        	 showSimpleToast("La categoria fue agregada correctamente a la base de datos.");
+	        }, function (err) {
+	          $scope.isLoading = false;
+	          showSimpleToast("Ha ocurrido un error. La categoria no pudo ser agregada a la base de datos.");
+	        }
+	      );
+	     
+  };
 
 	function deleteCategory(name,$http) {
 	 	$scope.isLoading=true;
@@ -117,7 +101,7 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdT
         	.then(
               function (response) {
               	$scope.isLoading=false;
-              	console.log("Pasó");
+              	showSimpleToast("La categoría "+name+" ha sido eliminada con éxito");
                 getAllCategories();
               }, function (err) {
               	$scope.isLoading=false;
@@ -142,7 +126,17 @@ appmodule.controller('categoriasController',function($scope,$http,$mdDialog,$mdT
   };
 	
 
+  function showSimpleToast(message) {
+      	var toast = $mdToast.simple()
+			.textContent(message)
+			.action("OK")
+			.highlightAction(true)
+			.position("top right end");
+			$mdToast.show(toast)
+			.then(function(response){
 
+			})
+  };
 
 
 
